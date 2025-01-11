86590f7d532c8ae7375bac3d2d2bfc80b5ccd544
afsal <afsalmohammed393@gmail.com>
2025-01-11 14:40:50 +0530

    clone 2 finished

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// var test = document.getElementById("test");
-//
-// document.onscroll = function () {
-//   scrollTop = window.scrollY;
-//   test.innerHTML = scrollTop;
-//   allDivs = document.getElementsByTagName("div");
-//
-//   for (i = 0; i < allDivs.length; i++) {
-//     curDiv = allDivs[i];
-//
-//     // The code below makes the background color change when the
-//     // scroll top passes the 2/3 of the previous div.
-//
-//     heightBefore = 0;
-//     if (i > 0) {
-//       heightBefore = allDivs[i - 1].offsetHeight / 3;
-//     }
-//
-//     if (scrollTop > curDiv.offsetTop - heightBefore) {
-//       color = curDiv.getAttribute("data-color");
-//       document.body.style.background = color;
-//     }
-//   }
-// };
-
 var test = document.getElementById("test");
 
 document.onscroll = function () {
