cfac295a8ad54ae6228a50f7806c702f66f69267
afsal <afsalmohammed393@gmail.com>
2025-01-11 15:25:57 +0530

    more changes added Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Backstage Talks Clone 🌐
+
+## 🚀 Live Demo
+
+Check out the live version of the website:  
+[![View Website](https://img.shields.io/badge/Website-Live-blue?style=flat-square)](https://afxxl.github.io/BackStage-Talks-Clone/)
--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 var test = document.getElementById("test");
 
 document.onscroll = function () {
-  var scrollTop = window.scrollY; // Get the current scroll position
-  test.innerHTML = scrollTop; // Show the scroll position in the "test" div
-  var allDivs = document.getElementsByClassName("main"); // Select sections by their class
-
+  var scrollTop = window.scrollY;
+  test.innerHTML = scrollTop;
+  var allDivs = document.getElementsByClassName("main");
   for (var i = 0; i < allDivs.length; i++) {
     var curDiv = allDivs[i];
 
-    // The code below makes the background color change when the scroll top passes
-    // 2/3 of the previous div's height
     var heightBefore = 0;
     if (i > 0) {
-      heightBefore = allDivs[i - 1].offsetHeight / 3; // Adjust for previous div's height
+      heightBefore = allDivs[i - 1].offsetHeight / 3;
     }
 
-    // Check if the scroll position has passed the threshold
     if (scrollTop >= curDiv.offsetTop - heightBefore) {
-      var color = curDiv.getAttribute("data-color"); // Get the color from data attribute
+      var color = curDiv.getAttribute("data-color");
       if (color) {
-        document.body.style.background = color; // Change the background color
+        document.body.style.background = color;
       }
     }
   }
